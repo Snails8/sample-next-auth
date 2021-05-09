190f7c25574775fe470b2e09db05d12ffe668bed
S-tanishi <tanichi.u.n.20@gmail.com>
2021-05-09 20:57:32 +0900

    不要な処理の削除

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,10 @@
-import Link from 'next/link'
 import Layout from '../components/Layout'
 import {useRequireLogin} from "../hooks/useRequireLogin";
 import { useLoginUser} from "../hooks/useLoginUser";
 
 const IndexPage = () => (
   useRequireLogin();
+  
   const { loginUser, isAuthChecking } = useLoginUser()
 
 if(isAuthChecking) return (<div>ログイン情報を確認中…</div>);
@@ -15,7 +15,6 @@ return (
         <h1>認証サンプル</h1>
         <div>あなたのユーザー名は{loginUser.name}です</div>
     </Layout>
-)
 )
 
 export default IndexPage
